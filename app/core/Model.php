ff11e5cb8c2190db5baf51b8c244853537c4c4d5
yuriphacsi <yuri.phacsi@tecsup.edu.pe>
2026-06-11 16:44:22 -0500

    Implement base model and first role model

--- app/core/Model.php
+++ app/core/Model.php
@@ -2,7 +2,40 @@
 
 namespace App\Core;
 
+use PDO;
+
 class Model
 {
+    protected PDO $db;
+    protected string $table = '';
+    protected string $primaryKey = 'id';
 
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function all(): array
+    {
+        $sql = "SELECT * FROM {$this->table}";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+
+    public function find(int $id): array|false
+    {
+        $sql = "SELECT * FROM {$this->table}
+                WHERE {$this->primaryKey} = :id";
+
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->bindValue(':id', $id);
+
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
 }
--- index.php
+++ index.php
@@ -2,16 +2,10 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$router = require_once __DIR__ . '/config/routes.php';
+use App\Models\Rol;
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$rol = new Rol();
 
-$basePath = '/Edutech';
-
-if (str_starts_with($uri, $basePath)) {
-    $uri = substr($uri, strlen($basePath));
-}
-
-$uri = $uri ?: '/';
-
-$router->dispatch($uri);
+echo "<pre>";
+print_r($rol->all());
+echo "</pre>";
